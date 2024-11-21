ef9331f4df031f00cf2ea98b71fa00e763f81325
Emili Sophia Cordeiro <emilisophiassss@gmail.com>
2024-11-21 09:24:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-let menu = prompt(`Escolha uma forma de pagamento: 
+let menu;
+
+do {
+  // Apresenta o menu para o usuário
+  menu = prompt(`Escolha uma forma de pagamento: 
 1 - Cartão de crédito 
 2 - Cartão de débito 
 3 - Pix 
 4 - Boleto 
-5 - Encerrar`)
-do{
+5 - Encerrar`);
+
+  // Processa a escolha do usuário
   switch(menu){
-    case 1:
-      alert('Você escolheu Cartão de crédito')
-      break
-    case 2:
-      alert('Você escolheu Cartão de débito')
-      break
-    case 3:
-      alert('Você escolheu Pix')
-      break
-    case 4:
-      alert('Você escolheu Boleto')
-      break
+    case '1':
+      alert('Você escolheu Cartão de crédito');
+      break;
+    case '2':
+      alert('Você escolheu Cartão de débito');
+      break;
+    case '3':
+      alert('Você escolheu Pix');
+      break;
+    case '4':
+      alert('Você escolheu Boleto');
+      break;
+    case '5':
+      alert('Encerrando...');
+      break;
+    default:
+      alert('Opção inválida. Tente novamente.');
   }
-} while (menu != 5)
+} while (menu != '5');
